b6c770eb60a1b21712128d7ad7e24047a6b5552b
Alex Wilson <fonzmp@gmail.com>
2018-05-21 20:43:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,10 @@
 class Triangle
   # write code here
 
-  def initialize(length_one, length_two, length_three)
+  def initialize(length_one:, length_two:, length_three:)
     @length_one = length_one
     @length_two = length_two
     @length_three = length_three
   end
+
 end
