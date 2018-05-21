785cc7536583f463910355f8458e80a3b772a5a4
Alex Wilson <fonzmp@gmail.com>
2018-05-21 21:38:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
 
   def legal?
-    if !((@length_one + @length_two) > @length_three)
+    if !((@length_one + @length_two) > @length_three) || !((@length_one + @length_three) > @length_two) || !((@length_two + @length_three) > @length_one)
   end
 
   def kind
