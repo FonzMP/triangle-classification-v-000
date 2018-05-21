7e9ddb4fadee3ae7f3660352245a884427144c17
Alex Wilson <fonzmp@gmail.com>
2018-05-21 21:20:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
 
   def kind
-    if ((@length_one + @length_two) < @length_three) 
+    if ((@length_one + @length_two) > @length_three)  || ((@length_one + @length_three) > @length_two) || ((@length_two + @length_three) < @length_one)
     if @length_one > 0 && @length_two > 0 && @length_three > 0
       if @length_one == @length_two && @length_two == @length_three
         :equilateral
