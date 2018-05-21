ac41a405e62fdcac8de26fa63c65a7f77093bca3
Alex Wilson <fonzmp@gmail.com>
2018-05-21 21:41:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,13 +13,13 @@ class Triangle
     if ((@length_one + @length_two) > @length_three) && ((@length_one + @length_three) > @length_two) && ((@length_two + @length_three) > @length_one)
       true
     else
-      true
+      false
     end
   end
 
   def kind
     if @length_one > 0 && @length_two > 0 && @length_three > 0
-      if self.legal?
+      if self.legal? == true
         if @length_one == @length_two && @length_two == @length_three
           :equilateral
         elsif (@length_one == @length_two && @length_two != @length_three) || (@length_one == @length_three && @length_two != @length_three) || (@length_two == @length_three && @length_one != @length_two)
