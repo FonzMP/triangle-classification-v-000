ef21b1ec3d78406f6b4e78d69d22edd32e4451ab
Alex Wilson <fonzmp@gmail.com>
2018-05-21 20:49:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
 
   attr_reader :length_one, :length_two, :length_three
 
-  def initialize(length_one:, length_two:, length_three:)
+  def initialize(length_one, length_two, length_three)
     @length_one = length_one
     @length_two = length_two
     @length_three = length_three
