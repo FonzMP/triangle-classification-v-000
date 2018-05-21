7064ea35363a3706684a73c240c0a9c76c15fd37
Alex Wilson <fonzmp@gmail.com>
2018-05-21 20:50:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     if @length_one > 0 && @length_two > 0 && @length_three > 0
       if @length_one == @length_two && @length_two == @length_three
         "equilateral"
+      end
     else
       raise TriangleError
     end
