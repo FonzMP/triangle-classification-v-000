b96484e4810a552684c493c7f0c48fc9445a5b31
Alex Wilson <fonzmp@gmail.com>
2018-05-21 21:27:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,10 @@ class Triangle
     @length_three = length_three
   end
 
+  def checker?
+
+  end
+
   def kind
     if @length_one > 0 && @length_two > 0 && @length_three > 0
       if ((@length_one + @length_two) > @length_three)  || ((@length_one + @length_three) > @length_two) || ((@length_two + @length_three) < @length_one)
