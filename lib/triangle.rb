3c816444ae93e0ca4d28d68ad8a89e082c9bfd5c
Alex Wilson <fonzmp@gmail.com>
2018-05-21 20:54:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if @length_one > 0 && @length_two > 0 && @length_three > 0
       if @length_one == @length_two && @length_two == @length_three
         :equilateral
-      elsif (@length_one == @length_two && @length_two != @length_three) || (@length_one == @length_three && @length_two != @length_three)
+      elsif (@length_one == @length_two && @length_two != @length_three) || (@length_one == @length_three && @length_two != @length_three) || (@length_two == @length_three && @length_one != @length_two)
         :isosceles
       end
     else
