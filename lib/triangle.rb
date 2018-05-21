3f15c0acc50055caa983ce7503bfbfcaa0a77baa
Alex Wilson <fonzmp@gmail.com>
2018-05-21 21:21:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,19 +11,19 @@ class Triangle
 
   def kind
     if ((@length_one + @length_two) > @length_three)  || ((@length_one + @length_three) > @length_two) || ((@length_two + @length_three) < @length_one)
-    if @length_one > 0 && @length_two > 0 && @length_three > 0
-      if @length_one == @length_two && @length_two == @length_three
-        :equilateral
-      elsif (@length_one == @length_two && @length_two != @length_three) || (@length_one == @length_three && @length_two != @length_three) || (@length_two == @length_three && @length_one != @length_two)
-        :isosceles
-      else
-        :scalene
-      end
+      if @length_one > 0 && @length_two > 0 && @length_three > 0
+        if @length_one == @length_two && @length_two == @length_three
+          :equilateral
+        elsif (@length_one == @length_two && @length_two != @length_three) || (@length_one == @length_three && @length_two != @length_three) || (@length_two == @length_three && @length_one != @length_two)
+          :isosceles
+        else
+          :scalene
+        end
 
+      end
+    else
+      raise TriangleError
     end
-  else
-    raise TriangleError
-  end
   end
 
   class TriangleError < StandardError
