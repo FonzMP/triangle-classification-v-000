e4850faa540443b77ad36b60c97dc5e0df06935c
Alex Wilson <fonzmp@gmail.com>
2018-05-21 20:43:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,9 @@
 class Triangle
   # write code here
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
 end
